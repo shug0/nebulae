02149471aa2c2f5a7cf391d557e280d119f80736
Thomas Alberola <thomas@needacoffee.fr>
2015-10-27 18:49:44 +0100

    Repair bug for destroying element :+1:

--- api/services/PlayerService.js
+++ api/services/PlayerService.js
@@ -5,15 +5,14 @@ module.exports = {
       next(players);
     })
   },
-  addPlayer: function(playerVal, next) {
-    console.log(playerVal);
-    Player.create({value: playerVal}).exec(function(err, player) {
+  addPlayer: function(player, next) {
+    Player.create(player).exec(function(err, player) {
       if(err) throw err;
       next(player);
     })
   },
-  removePlayer: function(playerVal, next) {
-    Player.destroy({value: playerVal}).exec(function(err, player) {
+  removePlayer: function(player, next) {
+    Player.destroy(player.id).exec(function(err, player) {
       if(err) throw err;
       next(player);
     })
