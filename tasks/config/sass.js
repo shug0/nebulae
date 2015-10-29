9edaa6e6fcc1a19188383bd38baf4ab782e213a9
Thomas Alberola <thomas@needacoffee.fr>
2015-10-29 11:43:22 +0100

    Adding SCSS + Remove LESS
    - Adding SCSS support with grunt-sass
    - Updating package.json

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -3,26 +3,29 @@
  *
  * ---------------------------------------------------------------
  *
- * Only the `assets/styles/importer.less` is compiled.
+ * Only the `assets/styles/importer.scss` is compiled.
  * This allows you to control the ordering yourself, i.e. import your
  * dependencies, mixins, variables, resets, etc. before other stylesheets)
  *
  * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-less
+ * 		https://github.com/gruntjs/grunt-contrib-scss
  */
 module.exports = function(grunt) {
 
-	grunt.config.set('less', {
+	grunt.config.set('sass', {
 		dev: {
+			options: {
+            sourceMap: true
+        	},
 			files: [{
 				expand: true,
 				cwd: 'assets/styles/',
-				src: ['importer.less'],
+				src: ['main.scss'],
 				dest: '.tmp/public/styles/',
 				ext: '.css'
 			}]
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-sass');
 };
