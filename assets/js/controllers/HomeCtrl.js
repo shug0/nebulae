3b8bef9b6d4a2b32a530628eb7f86cb78555dc92
corentin <corentin>
2015-11-27 16:14:40 +0100

    ajout et fenetre gridster ok

--- assets/js/controllers/HomeCtrl.js
+++ assets/js/controllers/HomeCtrl.js
@@ -1,12 +1,41 @@
 'use strict';
 
-NebulaeApp.controller('UserCtrl', ['$scope', '$rootScope', 'HomeSrv', function($scope, $rootScope, UserSrv) {
-  $scope.user = {};
-  $scope.users = [];
+NebulaeApp.controller('HomeCtrl', ['$scope', '$rootScope', 'UserSrv', function($scope, $rootScope, HomeSrv) {
 
-  UserSrv.getUsers().then(function(response) {
-    $scope.users = response;
-      console.log(response);
-  });
+    $scope.initGridster = function(){
+        console.log("init");
+
+            $scope.gridster = $(".gridster > ul").gridster({
+                widget_margins: [10, 10],
+                widget_base_dimensions: [400, 150],
+                resize: {
+                    enabled: true
+                }
+            }).data('gridster');
+
+            var widgets = [
+                ['<li>Widget Exemple</li>', 1, 2]
+            ];
+
+
+        $.each(widgets, function(i, widget){
+            $scope.gridster.add_widget.apply($scope.gridster, widget)
+        });
+
+    };
+
+    $scope.AddWindow = function() {
+       // console.log("add");
+        var fenetre = ['<li><div class=test>Nouveau Widget</div></li>', 1, 2];
+        $scope.gridster.add_widget.apply( $scope.gridster,fenetre);
+    };
+
+    $scope.DeleteWindow = function($id){
+        var fenetre = document.getElementById($id);
+        console.log(fenetre);
+        this.remove_widget.apply(fenetre);
+    };
+
+    $scope.initGridster();
 
 }]);
