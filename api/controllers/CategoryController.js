45bda9ed74ff05a3359b758bf0a4b6ba48868e21
cdupland <cyril.dupland@gmail.com>
2016-01-27 15:53:56 +0100

    fix ctrl

---
+++ api/controllers/CategoryController.js
@@ -0,0 +1,11 @@
+/**
+ * CategorieController
+ *
+ * @description :: Server-side logic for managing categories
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
